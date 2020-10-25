528e3e07876bd2e0cd50a3d701ab0790f9662f30
Ahmed <ahmed.3laa.2020@gmail.com>
2020-10-25 14:09:51 +0200

    install lotti library && and lottie reuseable component && show and render it with our app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LottieAnimation from "./Lottie";
+import womenPhone from "./animation/women-phone.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <LottieAnimation lotti={womenPhone} height={700} width={700} />
+        </div>
       </header>
     </div>
   );
